e38f8fc1bdc785954fc40615a50300cf9350f1d5
Suryajeet Bhosale <89635688+SuryajeetBhosale@users.noreply.github.com>
2021-09-30 23:29:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const app = express();
 app.use(express.static("public/"));
 
 app.get("", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html")); //Homepage
 });
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/about.html"));
+  res.sendFile(path.join(__dirname, "public/about.html")); //Aboutpage
 });
 app.get("/contact-me", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/contact-me.html"));
+  res.sendFile(path.join(__dirname, "public/contact-me.html")); //Contactmepage
 });
 app.listen(process.env.PORT || 8080, () => {
   console.log("Running!");
